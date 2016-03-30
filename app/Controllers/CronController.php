0f296a12fe050b5c2d03ae6d05340eb359329862
Jon Sherrard <jon@andthats.it>
2016-03-30 18:08:38 +0100

    testing imports

--- app/Controllers/CronController.php
+++ app/Controllers/CronController.php
@@ -14,15 +14,18 @@ class CronController {
   }
 
   public function tick() {
-    die;
     $posts_array = $this->sitemap->get_all_posts();
     foreach($posts_array as $post) {
       $slug = $this->return_slug($post);
       $post_object = get_page_by_path($slug, 'OBJECT', 'post');
-      if (!$post_object->catfish_importer_imported) {
+      if (!$post_object) {
         $check = Sync::importUrl($post);
         if ($check->success) {
           $this->notify->post_import_complete($check->post->id);
+          print_r($check);
+          exit;
+        } else {
+          print_r($check);exit;
         }
       }
     }
