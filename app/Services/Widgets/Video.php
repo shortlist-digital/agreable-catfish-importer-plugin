0a99aec75d057f63a654c62ae2d4b80a6ae93268
Jon Sherrard <jon@andthats.it>
2016-06-22 16:36:47 +0100

    fix youtube and vimeo embeds

--- app/Services/Widgets/Video.php
+++ app/Services/Widgets/Video.php
@@ -13,9 +13,32 @@ class Video {
     if (!isset($videoIframe[0])) {
       $widgetData->embed = $facebookVideo[0]->{'data-href'};
     } else {
-      $widgetData->embed = $videoIframe[0]->src;
+      $src = self::filterFrameSrc($videoIframe[0]->src);
+      $widgetData->embed = $src;
     }
 
     return $widgetData;
+  }
+
+  public static function filterFrameSrc($url) {
+		if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+			$url = str_replace("//","", $url);
+			$url = "http://" . $url;
+    }
+  	if (preg_match('/youtube\.com\/watch\?v=([^\&\?\/]+)/', $url, $id)) {
+			$values = $id[1];
+		} else if (preg_match('/youtube\.com\/embed\/([^\&\?\/]+)/', $url, $id)) {
+			$values = $id[1];
+		} else if (preg_match('/youtube\.com\/v\/([^\&\?\/]+)/', $url, $id)) {
+			$values = $id[1];
+		} else if (preg_match('/youtu\.be\/([^\&\?\/]+)/', $url, $id)) {
+			$values = $id[1];
+		}
+		else if (preg_match('/youtube\.com\/verify_age\?next_url=\/watch%3Fv%3D([^\&\?\/]+)/', $url, $id)) {
+				$values = $id[1];
+		} else {
+			return $url;
+		}
+		return "https://www.youtube.com/watch?v=".$values;
   }
 }
