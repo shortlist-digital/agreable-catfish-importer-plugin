23dfc9c29b6cba525715b4f1ca219071d80149c8
Jon Sherrard <jon@andthats.it>
2016-07-05 16:34:46 +0100

    strip spans and center tags

--- app/Services/Widgets/Paragraph.php
+++ app/Services/Widgets/Paragraph.php
@@ -7,7 +7,18 @@ class Paragraph {
   public static function getFromWidgetDom($widgetDom) {
     $widgetData = new stdClass();
     $widgetData->type = 'paragraph';
+    $widgetDom = self::filterBadTags($widgetDom);
     $widgetData->paragraph = $widgetDom->innertext;
     return $widgetData;
+  }
+
+  public static function filterBadTags($html) {
+    $badTags = 'span, center';
+    if (count($html->find($badTags))) {
+      foreach($html->find($badTags) as $index=>$element) {
+        $html->find($badTags, $index)->outertext = $element->innertext;
+      }
+    }
+    return $html;
   }
 }
