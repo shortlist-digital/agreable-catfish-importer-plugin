40f0da04393385b38777128c3b6be72eb7a7b97e
Ben Bowler <ben@benbowler.com>
2017-02-22 12:32:25 +0000

    Refactor the Posts class giving a 2.2x speed increase by removing Mesh and combining inserts

--- app/Services/Post.php
+++ app/Services/Post.php
@@ -31,90 +31,123 @@ class Post {
 
     if (!$object = json_decode($postString)) {
       self::notifyError('Unable to retrieve JSON from URL ' . $postJsonUrl);
-      $fail = true;
+      return false;
     }
 
     if (!isset($object->article)) {
       self::notifyError('"Article" property does not exist in JSON, might be a full page embed or microsite');
-      $fail = true;
-    }
-
-    if ($fail) {
       return false;
     }
 
-    $postObject = $object->article;
-    $postDom = HtmlDomParser::str_get_html($object->content);
-    $postReformatted = new stdClass();
+    // XXX: Create master post array to save into Wordpress
 
-    $meshPost = new \Mesh\Post($postObject->slug);
-    $meshPost->set('catfish_importer_url', $postUrl, true);
-    $meshPost->set('catfish_importer_imported', true, true);
-    $meshPost->set('catfish_importer_date_updated', time(), true);
-    $meshPost->set('post_title', $postObject->headline);
-    $meshPost->set('header_type', 'standard-hero');
-    $meshPost->set('header_display_headline', true);
-    $meshPost->set('header_display_sell', true);
+    $postObject = $object->article; // The article in object from as retrieved from Clock CMS API
+    $postDom = HtmlDomParser::str_get_html($object->content); // A parsed object of the post content to be split into ACF widgets as a later point
 
     // Set post published date
     $displayDate = strtotime($postObject->displayDate);
     $displayDate = date('o\-m\-d G\:i\:s', $displayDate);
 
-    wp_update_post(array(
-      'ID' => $meshPost->id,
+    // If no sell exists on this post then create it from the headline
+    $sell = empty($postObject->sell) ? $postObject->headline : $postObject->sell;
+
+    // Create the base array for the new Wordpress post
+    $postArrayForWordpress = array(
+      'post_name' => $postObject->slug,
+      'post_title' => $postObject->headline,
       'post_date' => $displayDate,
       'post_date_gmt' => $displayDate,
       'post_modified' => $displayDate,
       'post_modified_gmt' => $displayDate
-    ));
+    );
 
+    // Create or select Author ID
     if (isset($object->article->__author) &&
         isset($object->article->__author->emailAddress) &&
         $object->article->__author->emailAddress) {
 
-      $meshPost->set('post_author', self::setAuthor($object->article->__author));
+      $postArrayForWordpress['post_author'] = self::setAuthor($object->article->__author);
     } else {
       $get_author_details = get_field('catfish_default_author', 'option');
       $default_author = $get_author_details['ID'];
-      $meshPost->set('post_author', $default_author, true);
+      $postArrayForWordpress['post_author'] = $default_author;
     }
 
-    Category::attachCategories($object->article->section, $postUrl, $meshPost->id);
+    // Create meta array for new post (Data that's not in the core post_fields)
+    $postMetaArrayForWordpress = array(
+      'short_headline' => $postObject->shortHeadline,
+      'sell' => $sell,
+      'header_type' => 'standard-hero',
+      'header_display_headline' => true,
+      'header_display_sell' => true,
+      'catfish_importer_url' => $postUrl,
+      'catfish_importer_imported' => true,
+      'catfish_importer_date_updated' => time()
+    );
 
+    // If automated testing, set some metadata
+    if (isset($_SERVER['is-automated-testing'])) {
+      $postMetaArrayForWordpress['automated_testing'] = true;
+    }
+
+    // Save post and return ID of newly created post for updating Categories, tags and Widgets
+    $wpPostId = wp_insert_post($postArrayForWordpress);
+    // Save the post meta data (Any field that's not post_)
+    self::setPostMetadata($wpPostId, $postMetaArrayForWordpress);
+
+    // XXX: Actions to take place __after__ the post is saved and require either the Post ID or TimberPost object
+
+    // Attach Categories to Post
+    Category::attachCategories($object->article->section, $postUrl, $wpPostId);
+
+    // Attach
     $postTags = array();
     foreach($object->article->tags as $tag) {
       if ($tag->type !== 'System') {
         array_push($postTags, ucwords($tag->tag));
       }
     }
-    wp_set_post_tags($meshPost->id, $postTags);
+    wp_set_post_tags($wpPostId, $postTags);
 
-    $sell = !empty($postObject->sell) ? $postObject->sell : $postObject->headline;
-
-    $meshPost->set('short_headline', $postObject->shortHeadline, true);
-    $meshPost->set('sell', $sell, true);
-
-    $meshPost->set('catfish_importer_imported', true, true);
-
-    // If automated testing, set some metadata
-    if (isset($_SERVER['is-automated-testing'])) {
-      $meshPost->set('automated_testing', true, true);
-    }
-
-    if (!$post = new TimberPost($meshPost->id)) {
+    // Catch failure to create TimberPost object
+    if (!$post = new TimberPost($wpPostId)) {
       self::notifyError('Unexpected exception where Mesh did not create/fetch a post');
     }
 
-    $show_header = self::setHeroImages($post, $postDom, $postObject);
-
-    $meshPost->set('header_display_hero_image', $show_header);
-
+    // Create the ACF Widgets from DOM content
     $widgets = Widget::getWidgetsFromDom($postDom);
     Widget::setPostWidgets($post, $widgets, $postObject);
 
+    // Store header image
+    $show_header = self::setHeroImages($post, $postDom, $postObject);
+    $postArrayForWordpress['header_display_hero_image'] = $show_header;
+
+    // Envoke any actions hooked to the 'catfish_importer_post' tag
     do_action('catfish_importer_post', $post->ID);
 
     return $post;
+  }
+
+  /**
+   * Set or update multiple post meta properties at once
+   */
+  protected static function setPostMetadata($postId, $fields) {
+    foreach ($fields as $fieldName => $value) {
+      self::setPostMetaProperty($postId, $fieldName, $value);
+    }
+  }
+
+  /**
+   * Create or update a post meta field
+   */
+  protected static function setPostMetaProperty($postId, $fieldName, $value = '') {
+    if ( empty( $value ) OR ! $value ) {
+        delete_post_meta( $postId, $fieldName );
+    } elseif ( ! get_post_meta( $postId, $fieldName ) ) {
+        add_post_meta( $postId, $fieldName, $value );
+    } else {
+        update_post_meta( $postId, $fieldName, $value );
+    }
   }
 
   protected static function setAuthor($authorObject) {
