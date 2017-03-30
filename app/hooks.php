19fd522a25a14af2a9b7008ae406eb483bed34e6
Ben Bowler <ben@benbowler.com>
2017-03-30 16:20:42 +0100

    Hook up the Reimport button in the post listing

--- app/hooks.php
+++ app/hooks.php
@@ -5,6 +5,9 @@ namespace AgreableCatfishImporterPlugin\Hooks;
 use add_filter;
 use TimberPost;
 
+// Show the Reimport option in the posts listings
+//
+
 class ImporterHooks {
   function __construct() {
     add_filter('post_row_actions', array($this, 'add_retry_button'), 10, 2);
@@ -20,6 +23,7 @@ class ImporterHooks {
 
   public function get_html($post) {
     $link = $this->get_link($post);
+    // TODO only return if post has the meta catfish_importer_url
     return "<a title='Re-import from Catfish' class='reimport' href='$link'>Reimport</a>";
   }
 
