8b8546e1f3f863fdd1f9c8d723c6241a8f64d35b
maciekpaprocki <maciekpaprocki@users.noreply.github.com>
2017-06-26 14:07:17 +0100

    Removed herbert loading

--- plugin.php
+++ plugin.php
@@ -9,22 +9,3 @@
  * Author URI:        http://shortlistmedia.co.uk/
  * License:           MIT
  */
-
-if(file_exists(__DIR__ . '/vendor/getherbert/')){
-  require_once __DIR__ . '/vendor/autoload.php';
-} else {
-  require_once __DIR__ . '/../../../../vendor/autoload.php';
-}
-
-if(file_exists(__DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php')){
-  require_once __DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php';
-} else {
-  require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
-}
-
-// Load Mesh (non-autoloadable)
-if(file_exists(__DIR__ . '/vendor/jarednova/mesh/')){
-  require_once __DIR__ . '/vendor/jarednova/mesh/mesh.php';
-} else {
-  require_once __DIR__ . '/../../../../vendor/jarednova/mesh/mesh.php';
-}
